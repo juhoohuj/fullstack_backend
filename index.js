fc9ca53ec1d84ac357b53798d75e60b8dbe2b08d
Juho Ahonen <juho.ahonen2@gmail.com>
2023-12-07 15:26:47 +0200

    static added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+app.use(express.static('build'))
+
 app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan('tiny'), morgan(':body'))
