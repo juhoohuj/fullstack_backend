3d5639139df9c596840a99e84caf62e07182f546
Juho Ahonen <juho.ahonen2@gmail.com>
2023-12-07 23:15:06 +0200

    mongodb render connected

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.static('build'))
-
 app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan('tiny'), morgan(':body'))
 
-
-let  persons = [
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    },
-]
 
 
   app.get('/', (req, res) => {
@@ -28,71 +18,51 @@ let  persons = [
   })
   
   app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+      res.json(persons)
+    })
   })
 
   app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const person = persons.find(person => {
-      return person.id === id
-    })
-    console.log(person)
-    if ( person ) {
+    const person = Person.findById(id).then(person => {
       response.json(person)
-    }
-    else {
-      response.status(404).end()
-    }   
+    })
   })
 
   app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+    const person = Person.findByIdAndRemove(id).then(result => {
+      response.status(204).end()
+    })
   
-    response.status(204).end()
-  }) 
+  })
   
-  const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
   
   app.post('/api/persons', (request, response) => {
     const body = request.body
   
-    if (!body.name || !body.number) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
-
-    if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({ 
-          error: 'name must be unique' 
-        })
-      }
-      
-  
-    const person = {
+    
+    const person = new Person({
       name: body.name,
       number: body.number,
-      id: generateId(),
-    }
+    })
   
-    persons = persons.concat(person)
-  
-    response.json(person)
-
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
   })
 
   app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
-  }
-    )
+    Person.find({}).then(persons => {
+      res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+    })
+  })
 
-  const PORT = process.env.PORT || 3002
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
